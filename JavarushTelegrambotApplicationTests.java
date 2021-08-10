6f9d7c28477f17e737cfb03489835e58bc8ed5fc
Robert Takhnenko <86667932+RobTakh@users.noreply.github.com>
2021-08-10 23:27:47 +0300

    Add files via upload

---
+++ JavarushTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.RobTakh.JavarushTelegrambot;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
